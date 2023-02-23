8609e422f2f5179df895a9a609896a6b37f44bb4
hamzaahmedsiddiqui <dev.hamxa@gmail.com>
2023-02-23 22:10:33 +0100

    Added apk file to project and attached with the path

--- android/config/config.py
+++ android/config/config.py
@@ -1,11 +1,20 @@
 
+import os
+
+# Define the APK file name
+apk_filename = "com.fosanis.android.cancer_companion-1.8.30-4176-develop-debug.apk"
+# Getting the APK file path
+apk_path = os.path.join(os.path.dirname(os.getcwd()), 'resources', apk_filename)
 
 
+# Get the full path to the APK file
+#apk_path ="/Users/hamzaahmed/Documents/Mika/MikaTestAutomation/android/resources/com.fosanis.android.cancer_companion-1.8.30-4176-develop-debug.apk"
+print(apk_path)
 android_desire_capability = {
   "platformName": "Android",
   "appium:platformVersion": "12",
   "appium:automationName": "UiAutomator2",
-  "appium:app": "/Users/hamzaahmed/Documents/Mika/com.fosanis.android.cancer_companion-1.8.30-4176-develop-debug.apk",
+  "appium:app": apk_path,
   "appActivity": "com.fosanis.mika.app.MainActivity",
   "noReset": False
 }
